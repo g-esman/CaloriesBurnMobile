113903c169354d9c171ceab93f923e80bbc4041b
Gaston Esman <gastonesman@MacBook-Pro-de-Gaston.local>
2026-02-24 19:25:49 -0300

    instalacion inicial de RN, estrucuta de carpetas y config de linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,38 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      typescript: true,
+    },
+  },
+  rules: {
+    // Evita imports desordenados / rotos
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index'],
+          'object',
+          'type',
+        ],
+        pathGroups: [{ pattern: '@/**', group: 'internal' }],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+    // TS hygiene
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
 };
